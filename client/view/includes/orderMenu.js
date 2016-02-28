205d56ada71e51e26c025ed0c52bceca3e9b9ad1
DarkOverkill <maxym.fedas@gmail.com>
2016-02-29 00:09:16 +0200

    add edit item

--- client/view/includes/orderMenu.js
+++ client/view/includes/orderMenu.js
@@ -3,3 +3,17 @@ Template.orderMenu.helpers({
     return ItemsData.find();
   }
 });
+
+Template.orderMenu.events({
+  "click button[name='edit']": function(event, template){
+     console.log(this);
+     id = this._id;
+     $("input#itemName").val(this.name);
+     $("input#itemPrice").val(this.price);
+  },
+  "click button[name='saveChange']": function(event, template){
+    console.log(id);
+    ItemsData.update(id, {$set:{ name: $("input#itemName").val(), price: parseFloat($("input#itemPrice").val())
+    }});
+  }
+});
