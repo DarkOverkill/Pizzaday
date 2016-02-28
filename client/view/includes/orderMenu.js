cc87afacec4b083939324658651338463cf964bf
DarkOverkill <maxym.fedas@gmail.com>
2016-02-29 00:53:28 +0200

    add item insert

--- client/view/includes/orderMenu.js
+++ client/view/includes/orderMenu.js
@@ -12,8 +12,23 @@ Template.orderMenu.events({
      $("input#itemPrice").val(this.price);
   },
   "click button[name='saveChange']": function(event, template){
-    console.log(id);
-    ItemsData.update(id, {$set:{ name: $("input#itemName").val(), price: parseFloat($("input#itemPrice").val())
-    }});
+      if (typeof (id) !== "undefined") {
+        console.log(id);
+        ItemsData.update(id, {$set:{ name: $("input#itemName").val(), price: parseFloat($("input#itemPrice").val())
+      }});
+      delete id;
+    } else {
+      console.log("insert");
+      ItemsData.insert({name: $("input#itemName").val(), price: parseFloat($("input#itemPrice").val())});
+    }
+  },
+  "click button[name='cancel']": function(event, template){
+    delete id;
+    console.log(id == NaN);
+  },
+  "click input[name='addItem']": function(event, template){
+    console.log("add");
+    $("input#itemName").val("");
+    $("input#itemPrice").val("");
   }
 });
