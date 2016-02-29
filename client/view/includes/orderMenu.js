5315acf9e5b58fbb7033e322047d5c282deef425
DarkOverkill <maxym.fedas@gmail.com>
2016-02-29 20:59:21 +0200

    add delete item

--- client/view/includes/orderMenu.js
+++ client/view/includes/orderMenu.js
@@ -14,21 +14,24 @@ Template.orderMenu.events({
   "click button[name='saveChange']": function(event, template){
       if (typeof (id) !== "undefined") {
         console.log(id);
-        ItemsData.update(id, {$set:{ name: $("input#itemName").val(), price: parseFloat($("input#itemPrice").val())
+        ItemsData.update(id, {$set:{ name: $("input#itemName").val(), price: parseFloat($("input#itemPrice").val()).toFixed(2)
       }});
       delete id;
     } else {
       console.log("insert");
-      ItemsData.insert({name: $("input#itemName").val(), price: parseFloat($("input#itemPrice").val())});
+      ItemsData.insert({name: $("input#itemName").val(), price: parseFloat($("input#itemPrice").val()).toFixed(2)});
     }
   },
   "click button[name='cancel']": function(event, template){
     delete id;
-    console.log(id == NaN);
   },
   "click input[name='addItem']": function(event, template){
     console.log("add");
     $("input#itemName").val("");
     $("input#itemPrice").val("");
+  },
+  "click button[name='delete']": function(event, template){
+    console.log("remove");
+    ItemsData.remove(this._id);
   }
 });
