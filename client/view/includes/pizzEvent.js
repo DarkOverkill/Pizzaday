139306fb6e8a45032664da83bc846fe2f30d0b93
DarkOverkill <maxym.fedas@gmail.com>
2016-03-11 14:11:35 +0200

    pizzaEvent collection add

--- client/view/includes/pizzEvent.js
+++ client/view/includes/pizzEvent.js
@@ -1,5 +1,21 @@
 Template.pizzaEvent.events({
   "click button[name='create']": function(event, template){
-     alert();
+     var eventName = $('input#eventName').val();
+     var onDate = new Date($('input#onDate').val());
+     //console.log(eventName + "\t" + onDate);
+     //$('input#onDate2').val(onDate.toISOString().split('T')[0]);
+     PizzaEvent.insert({name: eventName,
+       onDate: onDate.toISOString().split('T')[0],
+       group: Meteor.user().profile.group,
+       status: "ordering",
+       eventCreator: Meteor.user().username});
+  }
+});
+Template.pizzaEvent.helpers({
+  events: function(){
+    PizzaEvent.find({group: Meteor.user().profile.group});
+  },
+  groupMenu: function(){
+    return ItemsData.find({group: /*Meteor.user().profile.group*/""});
   }
 });
