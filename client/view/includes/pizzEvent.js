65eda5dabfee1befdd7845e29a7dd4a93b81fedf
DarkOverkill <maxym.fedas@gmail.com>
2016-03-14 00:30:29 +0200

    add button see order

--- client/view/includes/pizzEvent.js
+++ client/view/includes/pizzEvent.js
@@ -19,15 +19,30 @@ Template.pizzaEvent.events({
     var itemCount = $('input[name="itemCount-' + eventId + '"]').val();
     PizzaEvent.update({_id: eventId}, {$push: {order: {"itemId": itemId, "count": itemCount, "user": Meteor.user().username}}});
     //console.log(ItemsData.findOne({_id: itemId}).name + "\t" + itemCount);
+  },
+
+  "click button#seeOrder" : function(event, template){
+    $('input[name="eventIdtoSeeOrder"]').val(this._id);
+    //Template.pizzaEvent.Foo("event");
   }
 });
 Template.pizzaEvent.helpers({
   events: function(){
-    return PizzaEvent.find({group: Meteor.user().profile.group});
+    return PizzaEvent.find({group: Meteor.user().profile.group}, {sort: {onDate: 1}});
   },
   groupMenu: function(){
     return ItemsData.find({group: Meteor.user().profile.group});
   },
+  orderedItems : function(){
+    //var eventId = $('input[name="eventIdtoSeeOrder"]').val();
+    //var returnOrder = [];
+    //var order = PizzaEvent.find({_id: eventId, group: Meteor.user().profile.group}, {sort: {onDate: 1}}).order;
+    //console.log(order.length);
+    //Template.pizzaEvent.Foo("helper");
+    //for (var i = 0; i < order.length; i++){
+    //
+    //}
+  }
   /*eventCreator: function(){
     var event = PizzaEvent.find({group: Meteor.user().profile.group}).fetch();
     var currUser = Meteor.user().username;
@@ -38,6 +53,16 @@ Template.pizzaEvent.helpers({
     return creators;
   }*/
 });
+/*
+Template.pizzaEvent.Foo = function(a){
+  console.log(a);
+};
+
+Template.pizzaEvent.created = function (a) {
+
+  Template.pizzaEvent.Foo(a);
+};
+*/
 Template.registerHelper("compare", function(v1, v2){
   if (typeof v1 === 'object' && typeof v2 === 'object') {
     return _.isEqual(v1, v2); // do a object comparison
